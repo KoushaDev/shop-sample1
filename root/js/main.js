257a03868db8733b1d1a10aa9b1e0314cd34eae2
unicape <unicape.social@gmail.com>
2022-09-01 18:43:40 +0430

    ...

---
+++ root/js/main.js
@@ -0,0 +1,11 @@
+
+let answers = document.querySelectorAll(".accordion");
+answers.forEach((event) => {
+  event.addEventListener("click", () => {
+    if (event.classList.contains("active")) {
+      event.classList.remove("active");
+    } else {
+      event.classList.add("active");
+    }
+  });
+});
